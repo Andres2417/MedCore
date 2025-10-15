2a00cc331ee4500998fa5e2c027a08f5a9e52517
Usuario <andresfb2002@gmail.com>
2025-10-15 11:56:03 -0500

    index updated es Modules

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-require("dotenv").config();
-const express = require('express')
-const database = require("./config/database")
-const routes = require("./router/routes")
-const bodyparser = require("body-parser")
-const cors = require("cors")
+import 'dotenv/config'; // equivalente a require('dotenv').config()
+import express from 'express';
+import database from './config/database.js';
+import routes from './router/routes.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware CORS
-app.use(cors({
-    origin: ["http://localhost:5173", "https://med-core-ewen0o414-andres2417s-projects.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://med-core.vercel.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
 // Middleware: POST, PUT, PATCH
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
 // Monta todas las rutas bajo el prefijo /api/v1/
-app.use('/v1/', routes)
+app.use('/v1/', routes);
 
 // Iniciar la conexión a la base de datos
 database();
 
-module.exports = app; 
+export default app;
