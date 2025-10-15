12adc32e2eb139e88b14f44db7612a766e977c25
Usuario <andresfb2002@gmail.com>
2025-10-14 20:23:21 -0500

    vercel fixed

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { uploadUsers } = require("../controllers/UserController");
+const { uploadUsers, getAllUsers } = require("../controllers/UserController");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/upload-users", upload.single("file"), uploadUsers);
-router.get("/all", require("../controllers/UserController").getAllUsers);
+router.get("/all", getAllUsers);
 
 module.exports = router;
