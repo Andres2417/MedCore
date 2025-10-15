df5ba198089db994eb770e2e5e2a3231b91fdbb4
Andres2417 <andresfb2002@gmail.com>
2025-10-15 07:35:18 -0500

    vercel fixed

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const multer = require("multer");
 const { uploadUsers, getAllUsers } = require("../controllers/UserController");
 
+// Usamos solo almacenamiento en memoria (MemoryStorage) para evitar el error EROFS
+// en el sistema de archivos de solo lectura de Vercel.
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/upload-users", upload.single("file"), uploadUsers);
