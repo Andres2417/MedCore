ad9af597f136ace325d30176ca4493f5309b20fb
Usuario <andresfb2002@gmail.com>
2025-10-14 20:02:01 -0500

    vercel fixed

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -3,15 +3,22 @@ import multer from "multer";
 import { uploadUsers, getAllUsers } from "../controllers/UserController.js";
 
 const router = express.Router();
-const storage = process.env.VERCEL
-  ? multer.memoryStorage()
-  : multer.diskStorage({
-      destination: 'uploads/',
-      filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
-    });
 
-const upload = multer({ storage });
+function getMulterStorage() {
+  // Vercel -> memoryStorage
+  if (process.env.VERCEL) {
+    return multer.memoryStorage();
+  }
 
+  // Entorno local -> diskStorage
+  return multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "uploads/"),
+    filename: (req, file, cb) =>
+      cb(null, Date.now() + "-" + file.originalname),
+  });
+}
+
+const upload = multer({ storage: getMulterStorage() });
 
 // Rutas
 router.post("/upload-users", upload.single("file"), uploadUsers);
