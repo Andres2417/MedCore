d409f738e83cab157e12b076c5e4025dd2a35d48
Usuario <andresfb2002@gmail.com>
2025-10-15 17:54:28 -0500

    test

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://med-core.vercel.app/',
+      'https://med-core.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
--- src/router/authRoutes.js
+++ src/router/authRoutes.js
@@ -4,6 +4,7 @@ import * as authController from "../controllers/AuthController.js";
 const router = express.Router();
 
 // Crear rutas para signup y login
+// https://med-core.vercel.app/api/v1/auth/login
 // http://localhost:3002/api/v1/auth/sign-up
 router.post("/sign-up", authController.signup);
 router.post("/verify-email", authController.verifyEmail);
